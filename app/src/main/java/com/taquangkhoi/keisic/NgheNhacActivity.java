2e64529a40a3e52a2afef867084370be5f0d1002
Tạ Quang Khôi <khoi0941@gmail.com>
2022-09-21 07:40:06 +0700

    Create act nghe_nhac and video

---
+++ app/src/main/java/com/taquangkhoi/keisic/NgheNhacActivity.java
@@ -0,0 +1,14 @@
+package com.taquangkhoi.keisic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NgheNhacActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nghe_nhac);
+    }
+}
