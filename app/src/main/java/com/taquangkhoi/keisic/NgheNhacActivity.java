30f6fdad28982c37097d49868a4c7804848e5de3
Tạ Quang Khôi <khoi0941@gmail.com>
2022-09-21 08:44:37 +0700

    Add many events

--- app/src/main/java/com/taquangkhoi/keisic/NgheNhacActivity.java
+++ app/src/main/java/com/taquangkhoi/keisic/NgheNhacActivity.java
@@ -2,13 +2,90 @@ package com.taquangkhoi.keisic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class NgheNhacActivity extends AppCompatActivity {
+    ImageView iv_play_stop;
+    ImageView iv_next;
+    ImageView iv_previous;
+    SeekBar sb_song;
+    TextView tv_current_duration;
+    TextView tv_duration;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nghe_nhac);
+
+        Control();
+        ThemNhac();
+        Events();
     }
+
+    private void ThemNhac() {
+    }
+
+
+    private void Events() {
+        RunSeekBar();
+        SetDuration();
+        iv_play_stop.setOnClickListener(v -> {
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.pause();
+                iv_play_stop.setImageResource(R.drawable.ic_play_24);
+            } else {
+                mediaPlayer.start();
+                iv_play_stop.setImageResource(R.drawable.ic_pause_24);
+            }
+        });
+    }
+
+    private void SetDuration() {
+        int duration = mediaPlayer.getDuration();
+        tv_duration.setText(String.format("%02d:%02d", duration / 1000 / 60, duration / 1000 % 60));
+    }
+
+    private void RunSeekBar() {
+        sb_song.setMax(mediaPlayer.getDuration());
+        sb_song.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    tv_current_duration.setText(String.format("%02d:%02d", progress / 1000 / 60, progress / 1000 % 60));
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+
+
+    private void Control() {
+        iv_play_stop = findViewById(R.id.iv_play_stop);
+        iv_next = findViewById(R.id.iv_next);
+        iv_previous = findViewById(R.id.iv_previous);
+        sb_song = findViewById(R.id.sb_song);
+        tv_current_duration = findViewById(R.id.tv_current_duration);
+        tv_duration = findViewById(R.id.tv_duration);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.rhymastic_y6u);
+
+    }
+
+
 }
