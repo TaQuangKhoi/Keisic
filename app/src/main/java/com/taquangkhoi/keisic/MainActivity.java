a7bc39ef3b583886e0ac77ceae27dc2fa74f338f
Tạ Quang Khôi <khoi0941@gmail.com>
2022-09-21 08:10:50 +0700

    Ánh xạ 2 nút xem và nghe

--- app/src/main/java/com/taquangkhoi/keisic/MainActivity.java
+++ app/src/main/java/com/taquangkhoi/keisic/MainActivity.java
@@ -3,12 +3,22 @@ package com.taquangkhoi.keisic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_nghe_nhac;
+    Button btn_xem_video;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AnhXa();
+    }
+
+    // Ánh xạ các view
+    private void AnhXa() {
+        btn_nghe_nhac = findViewById(R.id.btn_nghe_nhac);
+        btn_xem_video = findViewById(R.id.btn_xem_video);
     }
 }
