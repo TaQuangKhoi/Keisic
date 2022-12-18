15e2035ba204ca35762870ae52f1a28be4f3ff27
Tạ Quang Khôi <khoi0941@gmail.com>
2022-12-18 18:10:59 +0700

    Tạo inner class NotificationReceiver cho NotificationService

--- app/src/main/java/com/taquangkhoi/keisic/NotificationService.java
+++ app/src/main/java/com/taquangkhoi/keisic/NotificationService.java
@@ -1,5 +1,6 @@
 package com.taquangkhoi.keisic;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -72,5 +73,31 @@ public class NotificationService extends NotificationListenerService {
 
     public void setListener(MyListener myListener) {
         NotificationService.myListener = myListener;
+    }
+
+    class NotificationReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getStringExtra("command").equals("clearall")) {
+                NotificationService.this.cancelAllNotifications();
+            } else if (intent.getStringExtra("command").equals("list")) {
+                Intent i1 = new Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+                i1.putExtra("notification_event", "=====================");
+                sendBroadcast(i1);
+                int i = 1;
+
+                for (StatusBarNotification sbn : NotificationService.this.getActiveNotifications()) {
+                    Intent i2 = new Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+                    i2.putExtra("notification_event", i + " " + sbn.getPackageName() + "\n");
+                    sendBroadcast(i2);
+                    i++;
+                }
+
+                Intent i3 = new Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+                i3.putExtra("notification_event", "===== Notification List ====");
+                sendBroadcast(i3);
+            }
+        }
     }
 }
