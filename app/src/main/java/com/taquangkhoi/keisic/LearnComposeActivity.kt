3e6581984cd7d437f5b78991efda79c5cfb0312e
TaQuangKhoi <khoi0941@gmail.com>
2022-12-23 20:39:51 +0700

    Bắt đầu dùng thử Jetpack Compose

---
+++ app/src/main/java/com/taquangkhoi/keisic/LearnComposeActivity.kt
@@ -0,0 +1,36 @@
+package com.taquangkhoi.keisic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class LearnComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard(Message("Android", "Jetpack Compose"))
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Text(text = msg.author)
+    Text(text = msg.body)
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        Message(
+            "Colleague",
+            "Hey, take a look at Jetpack Compose, it's great!"
+        )
+    )
+}
