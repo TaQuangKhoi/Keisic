4d280c55c20fca172799b0f03a0e0c32e4affc0b
TaQuangKhoi <khoi0941@gmail.com>
2022-12-23 20:59:23 +0700

    Sử dụng color, MaterialTheme

--- app/src/main/java/com/taquangkhoi/keisic/LearnComposeActivity.kt
+++ app/src/main/java/com/taquangkhoi/keisic/LearnComposeActivity.kt
@@ -7,8 +7,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -36,18 +38,24 @@ fun MessageCard(msg: Message) {
                 .size(40.dp)
                 // Clip image to be shaped as a circle
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Box {
-                Text(text = msg.author)
-            }
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             // Add a vertical space between the author and message texts
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+            Text(
+                text = msg.body,
+                style = MaterialTheme.typography.body2
+            )
         }
     }
 
