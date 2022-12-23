6043def1f453ee8fcb7b03c3fa25fdafcaa23537
TaQuangKhoi <khoi0941@gmail.com>
2022-12-23 20:45:53 +0700

    Dùng thử Image của JC

--- app/src/main/java/com/taquangkhoi/keisic/LearnComposeActivity.kt
+++ app/src/main/java/com/taquangkhoi/keisic/LearnComposeActivity.kt
@@ -2,10 +2,15 @@ package com.taquangkhoi.keisic
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.res.painterResource
 
 class LearnComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +25,20 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Text(text = msg.author)
-    Text(text = msg.body)
+    Row {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture"
+        )
+
+        Column {
+            Box {
+                Text(text = msg.author)
+            }
+            Text(text = msg.body)
+        }
+    }
+
 }
 
 @Preview
